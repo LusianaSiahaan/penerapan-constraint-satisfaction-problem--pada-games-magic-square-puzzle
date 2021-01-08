d3506cc6a193834b4df7dfcaeb7e590c9cf27cab
Roy Gunawan Napitupulu <roy.gn70@gmail.com>
2021-01-08 20:02:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import subprocess
 
 while True:
     try:
-        choice = int(input("Please enter your choice: "))
+        choice = int(input("List of grid available:\n 1: Grid 3x3 \n 2: Grid 4x4 \n 3: Grid 5x5 \nChoose: "))
     except ValueError:
         print("Insert another value!")
         continue
     else:
         break
-if choice == 3: 
+if choice == 1: 
     subprocess.call(['python', 'grid_3x3.py'])
-elif choice == 4: 
+elif choice == 2: 
     subprocess.call(['python', 'grid_4x4.py'])
-elif choice == 5: 
+elif choice == 3: 
     subprocess.call(['python', 'grid_5x5.py'])
 else:
     print("Grid tidak ditemukan!")
